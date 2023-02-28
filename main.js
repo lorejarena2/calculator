05dd7e5e99ac942619b3e2da6600ea35a271cbee
lorejarena2 <lorejarena2@hotmail.com>
2023-02-28 13:24:06 -0500

    version 1 funcionalidad terminada correccion

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const num1=document.getElementById('uno');
 const num2=document.getElementById('dos');
 var mostrar=document.getElementById('mostrar')
 var operacion=document.getElementById('operacion')
+var operando=0
 
 
-function operaciones(operando){
+function operaciones(){
   switch(operando){
     case '-':
       resultado=parseInt(num1.value)-parseInt(num2.value);
@@ -26,14 +27,36 @@ function operaciones(operando){
       resultado=parseInt(num1.value)+parseInt(num2.value);
       mostrar.textContent=resultado;
       operacion.textContent='+' 
-      break;
-    case '.':
-      mostrar.textContent=0;
-      operacion.textContent='' 
-      num1.value=""
-      num2.value=""
-
-
       
+
   }
 }
+
+  function sum(){
+    operacion.textContent='+'
+    operando="+"
+  }
+
+  function rest(){
+    operacion.textContent='-'
+    operando="-"
+  }
+
+  function multi(){
+    operacion.textContent='*'
+    operando="*"
+  }
+
+  function divi(){
+    operacion.textContent='/'
+    operando="/"
+  }
+
+  function ac(){
+    mostrar.textContent=0;
+    operacion.textContent='' 
+    num1.value=""
+    num2.value=""
+  }
+
+  
