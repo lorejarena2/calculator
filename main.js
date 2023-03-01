0f80f60085e11e0541c623f6860c48e78602d2dc
lorejarena2 <lorejarena2@hotmail.com>
2023-03-01 13:59:12 -0500

    50% terminado

--- main.js
+++ main.js
@@ -1,80 +1,178 @@
-const num1=document.getElementById('numero');
-var mostrar=document.getElementById('mostrar')
-var operacion=document.getElementById('operacion')
+// const num1=document.getElementById('numero');
+// var mostrar=document.getElementById('mostrar')
+// var operacion=document.getElementById('operacion')
+var su=document.getElementById('btn_sumar')
 var operando=0
+var nom=0
 var uno=0
 var dos=0
+
+
+for (let i=0; i<=9; i++){
+  document.getElementById('btn_'+ i ).addEventListener('click',(evento)=> mostrar(evento.target));
+}
+
+function mostrar(tecla){
+  document.getElementById('mostrar').innerHTML=
+  document.getElementById('mostrar').innerHTML+tecla.value
+}
+
+function mostrar2(tecla){
+  document.getElementById('mostrar').innerHTML=
+  document.getElementById('mostrar').innerHTML+tecla
+}
+
+addEventListener('keypress',evento=>{
+  if(!isNaN(evento.key)){
+    mostrar2(evento.key)
+  }
+})
+
+
+addEventListener('keypress', (event) => {
+  if(isNaN(event.key) && (event.key != 'Enter')){
+  nom=event.key; 
+  operando=event.key;
+  uno=parseInt(document.getElementById('mostrar').innerHTML)
+  document.getElementById('mostrar').innerHTML=""
+  console.log(nom)
+  console.log(uno)
+  }
+ 
+ })
+
+
+addEventListener('keypress', (event) => {
+  if((event.key)=='Enter'){
+    console.log(uno)
+    teclado()
+  }
+ 
+ })
+
+
+ function teclado(){
+  switch(nom){
+    case '-':
+    dos=parseInt(document.getElementById('mostrar').innerHTML)
+    resultado=(uno-dos)
+    document.getElementById('mostrar').innerHTML=resultado;
+    console.log(uno)
+    console.log(dos) 
+    console.log('este es el resultado '+resultado)
+    operando=""
+    nom=""
+      break;
+   case '*':
+    dos=parseInt(document.getElementById('mostrar').innerHTML)
+    resultado=(dos*uno)
+    document.getElementById('mostrar').innerHTML=resultado;
+    console.log(uno)
+    console.log(dos) 
+    console.log('este es el resultado '+resultado)
+    operando=""
+    nom=""
+      break;
+    case '/':
+    dos=parseInt(document.getElementById('mostrar').innerHTML)
+     resultado=(uno/dos)
+     document.getElementById('mostrar').innerHTML=resultado;
+     console.log(uno)
+     console.log(dos) 
+     console.log('este es el resultado '+resultado)
+     operando=""
+     nom=""
+       break;
+    case '+':
+      dos=parseInt(document.getElementById('mostrar').innerHTML)
+      resultado=(dos+uno)
+      document.getElementById('mostrar').innerHTML=resultado;
+      console.log(uno)
+      console.log(dos)
+      console.log('este es el resultado '+resultado)
+      operando=""
+      nom=""
+      
+   }
+ }
 
 
 function operaciones(){
   switch(operando){
     case '-':
-    dos=parseInt(num1.value)
+    dos=parseInt(document.getElementById('mostrar').innerHTML)
     resultado=(uno-dos)
-    mostrar.textContent=resultado;
+    document.getElementById('mostrar').innerHTML=resultado;
     console.log(uno)
-    console.log(dos)
-    num1.value=""
+    console.log(dos) 
+    console.log('este es el resultado '+resultado)
     operando=""
+    nom=""
       break;
-    case '*':
-    dos=parseInt(num1.value)
+   case '*':
+    dos=parseInt(document.getElementById('mostrar').innerHTML)
     resultado=(dos*uno)
-    mostrar.textContent=resultado;
+    document.getElementById('mostrar').innerHTML=resultado;
     console.log(uno)
-    console.log(dos)
-    num1.value=""
+    console.log(dos) 
+    console.log('este es el resultado '+resultado)
     operando=""
+    nom=""
       break;
     case '/':
-    dos=parseInt(num1.value)
-    resultado=(uno/dos)
-    mostrar.textContent=resultado;
-    console.log(uno)
-    console.log(dos)
-    num1.value=""
-    operando=""
-      break;
-    case '+':
-      dos=parseInt(num1.value)
-      resultado=(dos+uno)
-      mostrar.textContent=resultado;
+    dos=parseInt(document.getElementById('mostrar').innerHTML)
+     resultado=(uno/dos)
+     document.getElementById('mostrar').innerHTML=resultado;
       console.log(uno)
+     console.log(dos) 
+     console.log('este es el resultado '+resultado)
+     operando=""
+     nom=""
+       break;
+    case '+':
+      dos=parseInt(document.getElementById('mostrar').innerHTML)
+      resultado=(dos+uno)
+      document.getElementById('mostrar').innerHTML=resultado;
+       console.log(uno)
       console.log(dos)
-      num1.value=""
+      console.log('este es el resultado '+resultado)
       operando=""
+      nom=""
       
-  }
-}
+   }
+ }
 
   function sum(){
     operando="+"
-    uno=parseInt(num1.value)
-    num1.value=""
-
+    uno=parseInt(document.getElementById('mostrar').innerHTML)
+    console.log(uno)
+    document.getElementById('mostrar').innerHTML=""
   }
 
-  function rest(){
+   function rest(){
     operando="-"
-    uno=parseInt(num1.value)
-    num1.value=""
+    uno=parseInt(document.getElementById('mostrar').innerHTML)
+    console.log(uno)
+    document.getElementById('mostrar').innerHTML=""
   }
 
   function divi(){
     operando="/"
-    uno=parseInt(num1.value)
-    num1.value=""
+    uno=parseInt(document.getElementById('mostrar').innerHTML)
+    console.log(uno)
+    document.getElementById('mostrar').innerHTML=""
   }
 
   function multi(){
     operando="*"
-    uno=parseInt(num1.value)
-    num1.value=""
-  }
+    uno=parseInt(document.getElementById('mostrar').innerHTML)
+    console.log(uno)
+    document.getElementById('mostrar').innerHTML=""
+   }
 
   function ac(){
-    mostrar.textContent=0;
-  }
+    document.getElementById('mostrar').innerHTML=""
+ }
 
 
 
